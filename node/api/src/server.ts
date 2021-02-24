3099bf8d3b4df62a9268f0861735ffdde5ae3c24
Andris Buscariolli <andrisbuscariolli88@gmail.com>
2021-02-23 22:15:20 -0300

    NODE - Aula 2

--- node/api/src/server.ts
+++ node/api/src/server.ts
@@ -1,22 +1,11 @@
+import 'reflect-metadata';
 import express, { response } from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-/**
- * GET => Buscar
- * PUSH => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello!' });
-});
-
-app.post('/', (req, res) => {
-  // recebe os dados para salvar
-  return res.json({ message: 'Os dados foram salvos com sucesso! ' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('Server is up!'));
